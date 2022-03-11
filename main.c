9d0650691cbfbef336fefac75405a00081ecd730
hakand41 <98481026+hakand41@users.noreply.github.com>
2022-03-11 15:32:43 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+	{
+	write(1, &c, 1);
+}
+
+void	rush(int x, int y)
+{
+	int	right;
+	int	down;
+
+	down = 1;
+	while (down <= y && x > 0)
+	{
+		right = 1;
+		while (right <= x)
+		{
+			if ((right == 1 && down == 1) || (right == x && down == 1))
+				ft_putchar('A');
+			else if ((right == 1 && down == y) || (right == x && down == y))
+				ft_putchar('C');
+			else if ((right == 1) || (right == x) || (down == 1) || (down == y))
+				ft_putchar('B');
+			else
+				ft_putchar(' ');
+			right++;
+		}
+		ft_putchar('\n');
+		down++;
+	}
+}
+
+int	main(void)
+{
+	rush(5, 5);
+	return (0);
+}
